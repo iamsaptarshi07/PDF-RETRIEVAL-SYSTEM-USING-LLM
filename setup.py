81cab9b03b4c1e8b9b1b63ba1b832409d274da14
Saptarshi <iamsaptarshi07@gmail.com>
2024-08-10 21:35:36 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Genarative AI PDF Retrival Project',
+    version = '0.0.0',
+    author = 'Saptarshi Mandal',
+    packages = find_packages(),
+)
